f950c3aea3737121da32b8930f2dbfb85e91a4c2
teplorobot <hostmaster@te-st.ru>
2015-05-13 15:03:02 +0000

    auto commit

---
+++ sodeyst/wp-content/themes/start/sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package bb
+ */
+
+
+?>
+
+<div id="secondary" class="widget-area bit md-4" role="complementary">
+	<div class="sidebar-wide">
+		<?php dynamic_sidebar( 'right-sidebar' ); ?>
+	</div>
+</div><!-- #secondary -->
