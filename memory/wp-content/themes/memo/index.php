fc8e55aa6d7ab9c8ca110e7a5a627b9e9f803f16
teplorobot <hostmaster@te-st.ru>
2015-05-14 22:00:02 +0000

    auto commit

--- memory/wp-content/themes/memo/index.php
+++ memory/wp-content/themes/memo/index.php
@@ -2,49 +2,45 @@
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package bb
  */
 
+global $wp_query;
+ 
 get_header(); ?>
 
 <?php get_template_part('partials/title', 'section');?>	
 
-<div class="frame">
-	<div id="primary" class="content-area bit md-8">
-		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					if(is_search())
-						get_template_part( 'partials/content', 'search' );
-					else
-						get_template_part( 'partials/content', get_post_type() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php memo_paging_nav(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'partials/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-	<?php get_sidebar(); ?>
+<?php if(is_post_type_archive(array('document', 'book')) || is_home()) { ?>
+<div class="complex-loop">
+	<div class="frame">
+	<?php
+		if(have_posts()){
+			$count = 1;
+			while(have_posts()){			
+				
+				if($count > ceil(count($wp_query->posts)/3)){
+					$count = 1;
+					echo "</div>";
+				}
+				
+				if($count == 1)
+					echo "<div class='bit md-4'>";
+				
+				the_post();
+				get_template_part( 'partials/content', get_post_type() );
+				$count++;			
+			}
+			echo "</div>";
+		}
+	?>
+	</div>
+	<?php memo_paging_nav(); ?>
 </div>
+<?php
+	}
+	else {
+		
+	}
+?>
 
 <?php get_footer(); ?>
