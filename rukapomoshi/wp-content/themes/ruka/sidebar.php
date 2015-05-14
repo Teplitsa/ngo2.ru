583068b7d9dcc687f2b4fb80f42da296eead70a0
teplorobot <hostmaster@te-st.ru>
2015-05-14 14:00:04 +0000

    auto commit

---
+++ rukapomoshi/wp-content/themes/ruka/sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package bb
+ */
+
+
+?>
+
+<div id="secondary" class="widget-area bit md-4 lg-3 lg-offset-1" role="complementary">
+	<div class="sidebar-wide">
+		<?php dynamic_sidebar( 'right-sidebar' ); ?>
+	</div>
+</div><!-- #secondary -->
