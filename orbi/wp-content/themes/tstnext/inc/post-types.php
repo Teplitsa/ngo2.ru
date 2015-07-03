f756dea692ed69ae3a310c2c1949808765b7d8cd
teplorobot <hostmaster@te-st.ru>
2015-07-03 20:00:03 +0000

    auto commit

--- orbi/wp-content/themes/tstnext/inc/post-types.php
+++ orbi/wp-content/themes/tstnext/inc/post-types.php
@@ -1,59 +1,59 @@
 <?php
-//add_action('init', 'tst_custom_content', 20);
+add_action('init', 'tst_custom_content', 20);
 if( !function_exists('tst_custom_content') ) {
 function tst_custom_content(){
 
     /** Existing post types settings: */
-    add_post_type_support('page', array('excerpt'));
+    deregister_taxonomy_for_object_type('post_tag', 'post');
 	
-	register_taxonomy('topics', array('post', 'reference'), array(
+	register_taxonomy('auctor', array('post',), array(
         'labels' => array(
-            'name'                       => 'Темы',
-            'singular_name'              => 'Тема',
-            'menu_name'                  => 'Темы',
-            'all_items'                  => 'Все темы',
-            'edit_item'                  => 'Редактировать тему',
+            'name'                       => 'Авторы',
+            'singular_name'              => 'Автор',
+            'menu_name'                  => 'Авторы',
+            'all_items'                  => 'Все авторы',
+            'edit_item'                  => 'Редактировать автора',
             'view_item'                  => 'Просмотреть',
-            'update_item'                => 'Обновить тему',
-            'add_new_item'               => 'Добавить новую тему',
-            'new_item_name'              => 'Название новой темы',
-            'parent_item'                => 'Родительская тема',
-            'parent_item_colon'          => 'Родительская тема:',            
-            'search_items'               => 'Искать темы',
+            'update_item'                => 'Обновить автора',
+            'add_new_item'               => 'Добавить нового автора',
+            'new_item_name'              => 'Название нового автора',
+            'parent_item'                => 'Родительский автор',
+            'parent_item_colon'          => 'Родительская автор:',            
+            'search_items'               => 'Искать авторов',
             'popular_items'              => 'Часто используемые',
             'separate_items_with_commas' => 'Разделять запятыми',
-            'add_or_remove_items'        => 'Добавить или удалить темы',
+            'add_or_remove_items'        => 'Добавить или удалить авторов',
             'choose_from_most_used'      => 'Выбрать из часто используемых',
             'not_found'                  => 'Не найдено'
         ),
-        'hierarchical'      => true,
+        'hierarchical'      => false,
         'show_ui'           => true,
-        'show_in_nav_menus' => true,
+        'show_in_nav_menus' => false,
         'show_tagcloud'     => false,
         'show_admin_column' => true,
         'query_var'         => true,
-        'rewrite'           => array('slug' => 'topics', 'with_front' => false),
+        'rewrite'           => array('slug' => 'auctor', 'with_front' => false),
         //'update_count_callback' => '',        
     ));
 	
 	
     /** Post types: */
-    register_post_type('reference', array(
+    register_post_type('event', array(
         'labels' => array(
-            'name'               => 'Ссылки',
-            'singular_name'      => 'Ссылка',
-            'menu_name'          => 'Ссылки',
-            'name_admin_bar'     => 'Добавить ссылку',
-            'add_new'            => 'Добавить новую',
-            'add_new_item'       => 'Добавить ссылку',
-            'new_item'           => 'Новая ссылка',
-            'edit_item'          => 'Редактировать ссылку',
-            'view_item'          => 'Просмотр ссылки',
-            'all_items'          => 'Все ссылки',
-            'search_items'       => 'Искать ссылки',
-            'parent_item_colon'  => 'Родительская ссылка:',
-            'not_found'          => 'Ссылки не найдены',
-            'not_found_in_trash' => 'В корзине ссылки не найдены'
+            'name'               => 'События',
+            'singular_name'      => 'Событие',
+            'menu_name'          => 'События',
+            'name_admin_bar'     => 'Добавить событие',
+            'add_new'            => 'Добавить новое',
+            'add_new_item'       => 'Добавить событие',
+            'new_item'           => 'Новое событие',
+            'edit_item'          => 'Редактировать событие',
+            'view_item'          => 'Просмотр события',
+            'all_items'          => 'Все события',
+            'search_items'       => 'Искать события',
+            'parent_item_colon'  => 'Родительское событие:',
+            'not_found'          => 'События не найдены',
+            'not_found_in_trash' => 'В корзине события не найдены'
        ),
         'public'              => true,
         'exclude_from_search' => false,
@@ -65,11 +65,12 @@ function tst_custom_content(){
         //'query_var'           => true,
         'capability_type'     => 'post',
         'has_archive'         => false,
-        'rewrite'             => array('slug' => 'ref', 'with_front' => false),
+        'rewrite'             => array('slug' => 'event', 'with_front' => false),
         'hierarchical'        => false,
         'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-calendar',
         'supports'            => array('title', 'excerpt', 'editor', 'thumbnail'),
-        'taxonomies'          => array('post_tag'),
+        'taxonomies'          => array(),
     ));
 
 	
