4ebeca2eadc7129843e90f90b91cb74f467b5003
teplorobot <hostmaster@te-st.ru>
2015-07-21 05:00:03 +0000

    auto commit

--- orbi/wp-content/themes/tstnext/partials/payment-form.php
+++ orbi/wp-content/themes/tstnext/partials/payment-form.php
@@ -4,17 +4,22 @@
  * be careful on updates
  **/
 
+if(!defined('LEYKA_VERSION'))
+	return;
+
+global $leyka_current_pm;	
+	
 $active_pm = apply_filters('leyka_form_pm_order', leyka_get_pm_list(true));
 
 leyka_pf_submission_errors();?>
 
-<div id="leyka-payment-form" class="leyka-tpl-toggles">
+<div id="leyka-payment-form" class="leyka-custom-template">
 <?php
 	$counter = 0;
 	foreach($active_pm as $i => $pm) {
 	leyka_setup_current_pm($pm); 
-	$counter++;?>
-
+	$counter++;
+?>
 <div class="leyka-payment-option toggle <?php if($counter == 1) echo 'toggled';?> <?php echo esc_attr($pm->full_id);?>">
 <div class="toggle-trigger <?php echo count($active_pm) > 1 ? '' : 'toggle-inactive';?>">
     <?php echo leyka_pf_get_pm_label();?>
@@ -23,38 +28,60 @@ leyka_pf_submission_errors();?>
 <form class="leyka-pm-form" id="<?php echo leyka_pf_get_form_id();?>" action="<?php echo leyka_pf_get_form_action();?>" method="post">
 	
 	<div class="leyka-pm-fields">
-<?php
-	echo leyka_pf_get_amount_field();
-	echo leyka_pf_get_hidden_fields();	
-?>
+		
+	<!-- amount -->
+	<?php
+		$supported_curr = leyka_get_active_currencies();
+		$current_curr = $leyka_current_pm->get_current_currency();
+	?>
+	
+	<div class="leyka-field amount">
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label figure">	
+			<input type="text" name="leyka_donation_amount" class="required mdl-textfield__input" id="donate_amount_flex" value="<?php echo esc_attr($supported_curr[$current_curr]['amount_settings']['flexible']);?>">                
+			<label for="leyka_donation_amount" class="leyka-screen-reader-text mdl-textfield__label"><?php _e('Donation amount', 'leyka');?></label>
+			<span id="leyka_donation_amount-error" class="field-error mdl-textfield__error"></span>
+		</div>
+		<div class="currency">
+			<?php echo $leyka_current_pm->get_currency_field();?>
+		</div>
+	</div>
+		
+	<?php
+		//echo leyka_pf_get_amount_field();
+		echo leyka_pf_get_hidden_fields();	
+	?>
 	<input name="leyka_payment_method" value="<?php echo esc_attr($pm->full_id);?>" type="hidden" />
 	<input name="leyka_ga_payment_method" value="<?php echo esc_attr($pm->label);?>" type="hidden" />
-	<div class='leyka-user-data'>		
-		<!-- name -->
-		<div class="mdl-textfield mdl-js-textfield leyka-field name">
-			<input type="text" class="required mdl-textfield__input" name="leyka_donor_name" id="leyka_donor_name" value="">
-			<label for="leyka_donor_name" class="leyka-screen-reader-text mdl-textfield__label"><?php _e('Your name', 'leyka');?></label>		
-			<span id="leyka_donor_name-error" class="field-error mdl-textfield__error"></span>
-		</div>
-		
-		<!-- email -->
-		<div class="mdl-textfield mdl-js-textfield leyka-field email">
-			<input type="text" value="" id="leyka_donor_email" name="leyka_donor_email" class="required email">
-			<label class="leyka-screen-reader-text" for="leyka_donor_email">Ваш email</label>
-			<span class="field-error" id="leyka_donor_email-error"></span>
-		</div>
-	<?php		
-		//echo leyka_pf_get_pm_fields();
-	?>
+			
+	<!-- name -->
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label leyka-field name">
+		<input type="text" class="required mdl-textfield__input" name="leyka_donor_name" id="leyka_donor_name" value="">
+		<label for="leyka_donor_name" class="leyka-screen-reader-text mdl-textfield__label"><?php _e('Your name', 'leyka');?></label>		
+		<span id="leyka_donor_name-error" class="field-error mdl-textfield__error"></span>
 	</div>
 	
-<?php
-    //echo leyka_pf_get_recurring_field();
-    echo leyka_pf_get_agree_field();
-    echo leyka_pf_get_submit_field();
-?>
-	<div class="mdl-textfield mdl-js-textfield leyka-field">
-		<input type="submit" class="" id="leyka_donation_submit" name="leyka_donation_submit" value="<?php echo esc_attr($this->get_submit_label());?>" />
+	<!-- email -->
+	<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label leyka-field email">
+		<input type="text" value="" id="leyka_donor_email" name="leyka_donor_email" class="required email mdl-textfield__input">
+		<label class="leyka-screen-reader-text mdl-textfield__label" for="leyka_donor_email">Ваш email</label>
+		<span class="field-error mdl-textfield__error" id="leyka_donor_email-error"></span>
+	</div>
+	<?php //echo leyka_pf_get_pm_fields();	?>
+	
+	<!-- agree -->
+	<div class="leyka-field agree">
+		<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect checkbox" for="leyka_agree">
+			<input type="checkbox" name="leyka_agree" id="leyka_agree" class="leyka_agree required mdl-checkbox__input" value="1" />
+			<a class="leyka-legal-confirmation-trigger" href="#<?php echo $agree_id;?>">
+                <?php echo leyka_options()->opt('agree_to_terms_text');?>
+            </a>
+		</label>		
+		<p class="field-error mdl-textfield__error" id="leyka_agree-error"></p>
+	</div>	
+	      
+	<!-- submit -->	
+	<div class="leyka-field submit">
+		<input type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" id="leyka_donation_submit" name="leyka_donation_submit" value="Пожертвовать" />
 	</div>
 	
 <?php
@@ -69,18 +96,25 @@ leyka_pf_submission_errors();?>
 		echo '<ul class="leyka-pm-icons cf">'.implode('', $list).'</ul>';
 	}?>
 	</div> <!-- .leyka-pm-fields -->	
-
-<?php echo "<div class='leyka-pm-desc'>".apply_filters('leyka_the_content', leyka_pf_get_pm_description())."</div>"; ?>
-
+	
+	<div class="leyka-pm-desc">
+		<?php echo apply_filters('leyka_the_content', leyka_pf_get_pm_description()); ?>
+	</div>
+	
 </form>
 </div>
 </div>
 <?php }?>
 
-<?php if(leyka_options()->opt('show_campaign_sharing')) {
-    leyka_share_campaign_block();
-}
+<div id="leyka-agree-text" class="leyka-oferta-text">
+	<div class="leyka-modal-close">X</div>
+	<div class="leyka-oferta-text-frame">
+		<div class="leyka-oferta-text-flow">
+			<?php echo apply_filters('leyka_terms_of_service_text', leyka_options()->opt('terms_of_service_text'));?>
+		</div>
+	</div>
+</div>
 
-leyka_pf_footer();?>
+<?php leyka_pf_footer();?>
 
 </div><!-- #leyka-payment-form -->
